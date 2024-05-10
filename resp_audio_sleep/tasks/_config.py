8147d715d9cb8f625838d1a1e78267e11dcedc2f
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-05-10 15:06:39 +0200

    fix skipping condition

--- resp_audio_sleep/tasks/_config.py
+++ resp_audio_sleep/tasks/_config.py
@@ -2,14 +2,14 @@ TRIGGERS: dict[str, int] = {"target": 1, "deviant": 2}
 TRIGGER_TYPE: str = "arduino"
 TRIGGER_ARGS: str | None = None
 # sequence settings
-N_TARGET: int = 100
-N_DEVIANT: int = 100
+N_TARGET: int = 10
+N_DEVIANT: int = 0
 # sound settings
 FQ_TARGET: int = 440
 FQ_DEVIANT: int = 2000
 SOUND_DURATION: float = 0.2
 # detector settings
-ECG_HEIGHT: float = 0.98
+ECG_HEIGHT: float = 0.99
 ECG_DISTANCE: float = 0.3
 RESP_PROMINENCE: float = 20
 RESP_DISTANCE: float = 0.8
