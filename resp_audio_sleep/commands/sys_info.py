aadb3c9780402a3cb3068a63d7628724bc868c0c
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-06-12 11:15:54 +0200

    better structure and add a couple of CLI tests

--- resp_audio_sleep/commands/sys_info.py
+++ resp_audio_sleep/commands/sys_info.py
@@ -2,15 +2,15 @@ from __future__ import annotations
 
 import click
 
-from .. import sys_info as sys_info_function
+from .. import sys_info
 
 
-@click.command()
+@click.command(name="sys-info")
 @click.option(
     "--developer",
     help="display information for optional dependencies",
     is_flag=True,
 )
-def sys_info(developer: bool) -> None:
+def run(developer: bool) -> None:
     """Run sys_info() command."""
-    sys_info_function(developer=developer)
+    sys_info(developer=developer)
