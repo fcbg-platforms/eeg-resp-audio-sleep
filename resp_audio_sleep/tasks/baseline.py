baa4d237dc6aa822ad02f51976176e364d86062b
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2024-10-14 10:48:24 +0200

    add fake buffer filling to baseline

--- resp_audio_sleep/tasks/baseline.py
+++ resp_audio_sleep/tasks/baseline.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from stimuli.time import sleep
 
+from ..detector import _BUFSIZE
 from ..utils._checks import check_type
 from ..utils.logs import logger
 from ._config import TRIGGER_TASKS
@@ -20,6 +21,7 @@ def baseline(duration: float) -> None:
     if duration <= 0:
         raise ValueError("The duration must be strictly positive.")
     trigger = create_trigger()
+    sleep(_BUFSIZE)  # fake a buffer filling
     logger.info("Starting baseline block of %.2f seconds.", duration)
     trigger.signal(TRIGGER_TASKS["baseline"][0])
     sleep(duration)
